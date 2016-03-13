ab95b59e5588a9915ebfca1c664a403b2f6c7a2f
jcalleja <jcalleja@softstackfactory.org>
2016-03-13 00:51:51 +0000

    commit changes before die result stuff

--- www/js/controller.js
+++ www/js/controller.js
@@ -3,19 +3,20 @@ angular.module('starter.controllers', [])
 .controller('LandingCtrl', ['$scope', '$state',
     function($scope, $state) {
         $scope.realResults = [];
+        $scope.selectedValue = "1";
+        $scope.dieNumOptions = [{
+            num: 1
+        }, {
+            num: 2
+        }, {
+            num: 3
+        }, {
+            num: 4
+        }];
         $scope.data = {
             dieNumSelect: {},
             dieTypeSelect: {},
             modifier: '',
-            dieNumOptions: [{
-                num: 1
-            }, {
-                num: 2
-            }, {
-                num: 3
-            }, {
-                num: 4
-            }],
             dieTypeOptions: [{
                 dieType: 'D4'
             }, {
@@ -30,32 +31,33 @@ angular.module('starter.controllers', [])
                 dieType: 'D20'
             }],
         };
+
         $scope.rollSubmit = function() {
             $scope.rollResults = '';
             $scope.arrayStore = {};
             $scope.modifiedResults = function(max, min) {
-                $scope.rollResults = (Math.floor(Math.random() * max + min));
+                $scope.rollResults = (Math.floor(Math.random() * ((max + 1) - min) + min));
                 $scope.arrayStore = $scope.rollResults + $scope.data.modifier;
             };
 
-            switch ($scope.data.dieNumSelect) {
-                case '1':
-                    if ($scope.data.dieTypeSelect == 'D4') {
+            switch ($scope.data.dieNumSelect.num) {
+                case 1:
+                    if ($scope.data.dieTypeSelect.dieType == 'D4') {
                         $scope.modifiedResults(4, 1);
                     }
-                    if ($scope.data.dieTypeSelect == 'D6') {
+                    if ($scope.data.dieTypeSelect.dieType == 'D6') {
                         $scope.modifiedResults(6, 1);
                     }
-                    if ($scope.data.dieTypeSelect == 'D8') {
+                    if ($scope.data.dieTypeSelect.dieType == 'D8') {
                         $scope.modifiedResults(8, 1);
                     }
-                    if ($scope.data.dieTypeSelect == 'D10') {
+                    if ($scope.data.dieTypeSelect.dieType == 'D10') {
                         $scope.modifiedResults(10, 1);
                     }
-                    if ($scope.data.dieTypeSelect == 'D12') {
+                    if ($scope.data.dieTypeSelect.dieType == 'D12') {
                         $scope.modifiedResults(12, 1);
                     }
-                    if ($scope.data.dieTypeSelect == 'D20') {
+                    if ($scope.data.dieTypeSelect.dieType == 'D20') {
                         $scope.modifiedResults(20, 1);
                         if ($scope.rollResults == 20) {
                             $scope.arrayStore = "CRIT!";
@@ -67,53 +69,53 @@ angular.module('starter.controllers', [])
                     $scope.realResults.unshift($scope.arrayStore);
                     break;
 
-                case '2':
-                    if ($scope.data.dieTypeSelect == 'D4') {
-                        $scope.modifiedResults(7, 2);
+                case 2:
+                    if ($scope.data.dieTypeSelect.dieType == 'D4') {
+                        $scope.modifiedResults(8, 2);
                     }
-                    if ($scope.data.dieTypeSelect == 'D6') {
-                        $scope.modifiedResults(11, 2);
+                    if ($scope.data.dieTypeSelect.dieType == 'D6') {
+                        $scope.modifiedResults(12, 2);
                     }
-                    if ($scope.data.dieTypeSelect == 'D8') {
-                        $scope.modifiedResults(15, 2);
+                    if ($scope.data.dieTypeSelect.dieType == 'D8') {
+                        $scope.modifiedResults(16, 2);
                     }
-                    if ($scope.data.dieTypeSelect == 'D10') {
-                        $scope.modifiedResults(19, 2);
+                    if ($scope.data.dieTypeSelect.dieType == 'D10') {
+                        $scope.modifiedResults(20, 2);
                     }
-                    if ($scope.data.dieTypeSelect == 'D12') {
-                        $scope.modifiedResults(23, 2);
+                    if ($scope.data.dieTypeSelect.dieType == 'D12') {
+                        $scope.modifiedResults(24, 2);
                     }
-                    if ($scope.data.dieTypeSelect == 'D20') {
-                        $scope.modifiedResults(39, 2);
+                    if ($scope.data.dieTypeSelect.dieType == 'D20') {
+                        $scope.modifiedResults(40, 2);
                     }
                     $scope.realResults.unshift($scope.arrayStore);
                     break;
 
 
-                case '3':
-                    if ($scope.data.dieTypeSelect == 'D4') {
+                case 3:
+                    if ($scope.data.dieTypeSelect.dieType == 'D4') {
                         $scope.modifiedResults(10, 3);
                     }
-                    if ($scope.data.dieTypeSelect == 'D6') {
-                        $scope.modifiedResults(16, 3);
+                    if ($scope.data.dieTypeSelect.dieType == 'D6') {
+                        $scope.modifiedResults(18, 3);
                     }
-                    if ($scope.data.dieTypeSelect == 'D8') {
-                        $scope.modifiedResults(22, 3);
+                    if ($scope.data.dieTypeSelect.dieType == 'D8') {
+                        $scope.modifiedResults(24, 3);
                     }
-                    if ($scope.data.dieTypeSelect == 'D10') {
-                        $scope.modifiedResults(28, 3);
+                    if ($scope.data.dieTypeSelect.dieType == 'D10') {
+                        $scope.modifiedResults(30, 3);
                     }
-                    if ($scope.data.dieTypeSelect == 'D12') {
-                        $scope.modifiedResults(34, 3);
+                    if ($scope.data.dieTypeSelect.dieType == 'D12') {
+                        $scope.modifiedResults(36, 3);
                     }
-                    if ($scope.data.dieTypeSelect == 'D20') {
-                        $scope.modifiedResults(58, 3);
+                    if ($scope.data.dieTypeSelect.dieType == 'D20') {
+                        $scope.modifiedResults(60, 3);
                     }
                     $scope.realResults.unshift($scope.arrayStore);
                     break;
 
 
-                case '4':
+                case 4:
                     if ($scope.data.dieTypeSelect == 'D4') {
                         $scope.modifiedResults(13, 4);
                     }
