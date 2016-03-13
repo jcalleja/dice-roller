ca621eb9c9dea606a308a84a7d9d4758bc00736d
jcalleja <jcalleja@softstackfactory.org>
2016-03-13 00:58:01 +0000

    commit fixes to case 3+4

--- www/js/controller.js
+++ www/js/controller.js
@@ -94,7 +94,7 @@ angular.module('starter.controllers', [])
 
                 case 3:
                     if ($scope.data.dieTypeSelect.dieType == 'D4') {
-                        $scope.modifiedResults(10, 3);
+                        $scope.modifiedResults(12, 3);
                     }
                     if ($scope.data.dieTypeSelect.dieType == 'D6') {
                         $scope.modifiedResults(18, 3);
@@ -117,22 +117,22 @@ angular.module('starter.controllers', [])
 
                 case 4:
                     if ($scope.data.dieTypeSelect == 'D4') {
-                        $scope.modifiedResults(13, 4);
+                        $scope.modifiedResults(16, 4);
                     }
                     if ($scope.data.dieTypeSelect == 'D6') {
-                        $scope.modifiedResults(21, 4);
+                        $scope.modifiedResults(24, 4);
                     }
                     if ($scope.data.dieTypeSelect == 'D8') {
-                        $scope.modifiedResults(29, 4);
+                        $scope.modifiedResults(32, 4);
                     }
                     if ($scope.data.dieTypeSelect == 'D10') {
-                        $scope.modifiedResults(37, 4);
+                        $scope.modifiedResults(40, 4);
                     }
                     if ($scope.data.dieTypeSelect == 'D12') {
-                        $scope.modifiedResults(45, 4);
+                        $scope.modifiedResults(48, 4);
                     }
                     if ($scope.data.dieTypeSelect == 'D20') {
-                        $scope.modifiedResults(77, 4);
+                        $scope.modifiedResults(80, 4);
                     }
                     $scope.realResults.unshift($scope.arrayStore);
                     break;
